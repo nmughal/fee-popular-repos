b85ad2b317bccd4a691b4ef00dd50c94ea955813
Nadia Mughal <nmughal14@gmail.com>
2017-04-11 17:06:19 -0400

    Added the collapse function so that when header is clicked, the body is hidden

--- src/repos/gh-repo.directive.js
+++ src/repos/gh-repo.directive.js
@@ -5,14 +5,24 @@
         .directive('ghRepo', ghRepo);
 
     function ghRepo() {
+        let $ = angular.element;
 
         return {
             templateUrl: 'repos/gh-repo.template.html',
             restrict: 'E',
+            link: setupCollapse,
             scope: {
               repo: '='
             }
         };
+
+        function setupCollapse(scope, element) {
+            $(element)
+                .find('header')
+                .on('click', function hidePanelBody() {
+                  $(element).find('section').toggleClass('hidden');
+                });
+        }
 
     }
 
